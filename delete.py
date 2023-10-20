83b4569de52782b2c8553d32ee60d8f1cfceef09
Phillip Pollard <phil@crescendo.net>
2023-10-20 15:26:58 -0400

    * Now exiting if no deletes are made.
    * We're limiting the look for the more SVC (...) to only within the
      timeline via Xpath now. So all the extra work to handle weird issues
      from clicking on the wrong "..." is not necessary.
    
    * Sizing the window larger for a better view.

--- delete.py
+++ delete.py
@@ -13,6 +13,7 @@ from selenium.common.exceptions import ElementClickInterceptedException, Element
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options
 
 def get_credentials() -> dict:
     credentials = dict()
@@ -27,7 +28,11 @@ def get_credentials() -> dict:
     return credentials
 
 def login(creds):
-	brows = webdriver.Firefox()
+	firefox_options = Options()
+	firefox_options.add_argument('--width=1200')
+	firefox_options.add_argument('--height=1800')
+
+	brows = webdriver.Firefox(options = firefox_options)
 	brows.implicitly_wait(2)
 
 	brows.get(f'https://twitter.com/i/flow/login?redirect_after_login=%2F{creds["username"]}')
@@ -47,7 +52,7 @@ def login(creds):
 
 def find_more_links(brows):
 	more_links = []
-	svgs = brows.find_elements("xpath", '//*[name()="svg"]')
+	svgs = brows.find_elements("xpath", "//div[contains(@aria-label, 'Timeline') and contains(@aria-label, 'posts')]//*[name()='svg']")
 	for svg in svgs:
 		if "M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9" in svg.get_attribute('innerHTML'):
 			more_links.append(svg)
@@ -87,11 +92,11 @@ def main():
 		# Find the "..." SVG and click on it
 		more_links = find_more_links(brows)
 
-		if len(more_links) < 2:
+		if len(more_links) < 1:
 			print("No links to click and delete...")
 			break
 
-		more_links[1].click()
+		more_links[0].click()
 
 		didit = try_to_delete(brows)
 
@@ -111,15 +116,12 @@ def main():
 		brows.get(f'https://twitter.com/{creds["username"]}/with_replies')
 		time.sleep(2)
 
+		last_pass = reply_count
+
 		more_links = find_more_links(brows)
 
 		for link in more_links:
-
-			try:
-				link.click()
-			except (ElementClickInterceptedException, ElementNotInteractableException):
-				print('Whoops')
-				pass
+			link.click()
 
 			didit = try_to_delete(brows)
 
@@ -128,6 +130,10 @@ def main():
 				reply_count += 1
 			else:
 				actions.send_keys(Keys.ESCAPE).perform()
+
+		if last_pass == reply_count:
+			# We did a loop and had zero deletes
+			break
 
 	print(f'{reply_count} tweet replies deleted')
 	time.sleep(2)
